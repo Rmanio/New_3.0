ef063e3b0922574c25a8d7bdccf18a663d81f360
Rmanio <103020674+Rmanio@users.noreply.github.com>
2022-06-03 11:28:00 +0600

    a more bit changed to correct work admin panel with heroku,I changed URL with localhost to our heroku URL

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/ADMIN/api/users/${data.id}`,
+        "url" : `https://sheltered-gorge-13862.herokuapp.com/ADMIN/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -31,7 +31,7 @@ if(window.location.pathname === "/ADMIN"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/ADMIN/api/users/${id}`,
+            "url" : `https://sheltered-gorge-13862.herokuapp.com/ADMIN/api/users/${id}`,
             "method" : "DELETE"
         }
 
